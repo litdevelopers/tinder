ec08158290ad81c550a3dab30d8f82471c17c5f0
paulxuca <paulxuca@gmail.com>
2016-08-05 23:13:18 -0400

    Api endpoint for facebook auth set up

---
+++ server/api/api.js
@@ -0,0 +1,57 @@
+const router = require('express').Router(); // eslint-disable-line new-cap
+const tinder = require('tinder');
+const client = new tinder.TinderClient();
+const nightmare = require('nightmare');
+const n = nightmare({ show: false });
+
+
+const FACEBOOK_AUTH_URL = 'https://www.facebook.com/dialog/oauth?client_id=464891386855067&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=basic_info,email,public_profile,user_about_me,user_activities,user_birthday,user_education_history,user_friends,user_interests,user_likes,user_location,user_photos,user_relationship_details&response_type=token';
+const FACEBOOK_URL = 'https://facebook.com';
+
+router.post('/auth/facebook', (req, res) => {
+  console.log(req.body);
+  n
+    .goto(FACEBOOK_AUTH_URL)
+    .exists('form[id="login_form"]')
+    .then((formExists) => {
+      console.log(formExists);
+      if (!formExists) {
+        n
+          .url()
+          .then((noLoginUrl) => {
+            n.goto(FACEBOOK_URL)
+            .evaluate(() => {
+              return document.querySelector('a[title="Profile"] img').id.split('_').reverse()[0];
+            })
+            .then((profileId) => {
+              res.status(200).json({
+                token: noLoginUrl.split(/access_token=|&expires_in/)[1],
+                id: profileId,
+              });
+            });
+          });
+      } else {
+        n
+          .type('input[name="email"]', req.body.login)
+          .type('input[name="pass"]', req.body.password)
+          .click('button[id="loginbutton"]')
+          .wait(200)
+          .url()
+          .then((loginURL) => {
+            n.goto(FACEBOOK_URL)
+            .evaluate(() => {
+              return document.querySelector('a[title="Profile"] img').id.split('_').reverse()[0];
+            })
+            .then((profileId) => {
+              res.status(200).json({
+                token: loginURL.split(/access_token=|&expires_in/)[1],
+                id: profileId,
+              });
+            });
+          });
+      }
+    });
+});
+
+
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -9,9 +9,16 @@ const isDev = process.env.NODE_ENV !== 'production';
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const resolve = require('path').resolve;
 const app = express();
+const bodyParser = require('body-parser');
+const api = require('./api/api.js');
+
+// json middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
-// app.use('/api', myApi);
+app.use('/api', api);
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
