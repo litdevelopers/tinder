a4daa2bd337724d4fd415f8434fdea6cef07290c
Eddie Wang <eddie@eddiewang.me>
2016-08-13 12:15:33 -0400

    more styling

--- app/containers/Matches/index.js
+++ app/containers/Matches/index.js
@@ -33,8 +33,15 @@ class DashboardMatches extends React.Component { // eslint-disable-line
       <div className={styles.dashboardMatchesContainer}>
         <div className={styles.dashboardMatchesCards}>
           <div className={styles.dashboardMatchesNavigation}>
-            <Button type="fetchMatches" onClick={() => this.props.onMultiple(this.props.matches, 'like')}>Like All</Button>
-            <Button type="fetchMatches" onClick={() => this.props.onMultiple(this.props.matches, 'pass')}>Pass All</Button>
+            <select>
+              <option value="normal">Normal</option>
+              <option value="active">Recently Active</option>
+              <option value="nearby">Nearby</option>
+            </select>
+            <div>
+              <Button type="fetchMatches" onClick={() => this.props.onMultiple(this.props.matches, 'like')}>Like All</Button>
+              <Button type="fetchMatches" onClick={() => this.props.onMultiple(this.props.matches, 'pass')}>Pass All</Button>
+            </div>
           </div>
           <div ref="scrollContainer" className={styles.dashboardMatchesCardsContainer}>
             {matches}
